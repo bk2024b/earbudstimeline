316538e34d340452d4daee36929fbcd7a1b0a7a5
bk <josiasboco@gmail.com>
2026-08-14 11:18:14 +0100

    next intl installé

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+// next.config.mjs
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.js');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -7,4 +12,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
